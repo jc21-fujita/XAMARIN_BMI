4222035cad749c28038fd9bc21fb0f7ba03ecc87
大世 藤田 <160245@jc-21.jp>
2018-11-26 12:00:26 +0900

    プロジェクト ファイルを追加します。

---
+++ XAMARIN.BMI2/XAMARIN.BMI2/BmiResult.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMARIN.BMI2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BmiResult : ContentPage
+    {
+        private float height;
+        private int weight;
+
+        public BmiResult()
+        {
+            InitializeComponent();
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+        public BmiResult(float height,int weight)
+        {
+            InitializeComponent();
+            this.height = height;
+            this.weight = weight;
+            
+
+            if (height <= 3)
+            {
+                labelBMI.Text = "BMI=" + weight / (height * height);
+                float BMI = +weight / (height * height);
+            }
+            else
+            {
+                height = height / 100;
+                labelBMI.Text = "BMI=" + weight / (height * height);
+                float BMI = +weight / (height * height);
+            }
+
+        }
+
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ XAMARIN.BMI2/XAMARIN.BMI2/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XAMARIN.BMI2
+{
+    public partial class MainPage : ContentPage
+    { 
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ButtonBMI.Clicked += ButtonClickEvent;
+            ButtonBMI2.Clicked += ButtonClickEvent2;
+        }
+
+        public void ButtonClickEvent2(object sender, EventArgs e)
+        {
+            float height = float.Parse(EntryHeight.Text);
+            int weight = int.Parse(EntryWeight.Text);
+            Navigation.PushModalAsync(new NavigationPage(new BmiResult(height,weight)));
+        }
+
+        public void ButtonClickEvent(object sender, EventArgs e)
+        {
+            float height = float.Parse(EntryHeight.Text);
+            int weight = int.Parse(EntryWeight.Text);
+
+            if (height <= 3) {
+                LabelResult.Text = "BMI=" + weight / (height * height);
+                float BMI = +weight / (height * height);
+            }
+            else
+            {
+                height = height / 100;
+                LabelResult.Text = "BMI=" + weight / (height * height);
+                float BMI = +weight / (height * height);
+            }
+
+            
+
+        }
+    }
+}
